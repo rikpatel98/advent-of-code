5a938063321612756ea9a0f0da76c64160cd7c13
Rikesh Patel <rp1998@outlook.com>
2022-01-07 10:37:14 +0000

    code works im just stupid

--- Day 4/day4sol.py
+++ Day 4/day4sol.py
@@ -8,13 +8,25 @@ import numpy as np
 numbers = [76,69,38,62,33,48,81,2,64,21,80,90,29,99,37,15,93,46,75,0,89,56,58,40,92,47,8,6,54,96,12,66,83,4,70,19,17,5,50,52,45,51,18,27,49,71,28,86,74,77,11,20,84,72,23,31,16,78,91,65,87,79,73,94,24,68,63,9,88,82,30,42,60,13,67,85,44,59,7,53,22,1,26,41,61,55,43,39,3,35,25,34,57,10,14,32,97,95,36,98
 ]
 
+egnums = [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1]
+
 boards = np.genfromtxt('C:\\Users\\rikpatel\\advent-of-code\\Day 4\\input.txt', delimiter = None)
+example = np.genfromtxt('C:\\Users\\rikpatel\\advent-of-code\\Day 4\\example.txt', delimiter = None)
+fifteen = np.arange(0,15,5)
+print(fifteen)
 fives = np.arange(0,500,5)
 array_boards = []
 for i in fives:
     test = np.vstack([boards[i:i+5]])
     array_boards.append(test)
     
+egboards = []
+for i in fifteen:
+    board = np.vstack([example[i:i+5]])
+    egboards.append(board)
+    
+    
+print(egboards[2][4,3])
 
 #print(array_boards[0])
 
@@ -29,7 +41,26 @@ for i in fives:
 #if ([5,39,87,66,91]) == (array_boards[0][:,0]).tolist():
     #print('hi')
     
-
+for i in egnums:
+    for j in egboards:
+        
+        j[j == i] = -1
+            
+        for r in range(5):
+            if [-1,-1,-1,-1,-1] == j[r].tolist() or [-1,-1,-1,-1,-1] == j[:,r].tolist():
+                print(j)
+                
+                print(i)
+                break
+            else:
+                continue
+            break
+        else:
+            continue
+        break
+    else:
+        continue
+    break
 
 
 
@@ -56,6 +87,9 @@ for i in numbers:
     else:
         continue
     break
+
+ 
+
 
             
                 
